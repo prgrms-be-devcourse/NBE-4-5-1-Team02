4faf80ad2663ffc7c074c9f9db0b8a4c59e02e90
Kim Ji Woo <jiwoo22110103@gmail.com>
2025-02-17 08:57:49 +0900

    Initial commit
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/team2/demo/CaffeApplication.java
@@ -0,0 +1,13 @@
+package com.team2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaffeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaffeApplication.class, args);
+	}
+
+}
