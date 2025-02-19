1c136f681b203f50d2ffde84474c81cecc862c1f
yunjuKimm <yunju.tech@gmail.com>
2025-02-19 21:39:49 +0900

    Product 엔티티 클래스 작성

--- src/main/java/com/team2/demo/product/Product.java
+++ src/main/java/com/team2/demo/product/Product.java
@@ -1,4 +1,34 @@
-package com.team2.demo.product;
+package com.team2.demo.product; // Order 와 다른 패키지에 위치
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "PRODUCT")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Product {
+
+    @Id
+    @Column(name = "PRODUCT_UUID")
+    private String productUuid;
+
+    @Column(name = "PRODUCT_NAME")
+    private String productName;
+
+    @Column(name = "CATEGORY")
+    private String category;
+
+    @Column(name = "PRODUCT_PRICE")
+    private Integer productPrice;
+
+    @Column(name = "PRODUCT_DESCRIPTION")
+    private String productDescription;
+
+    @Column(name = "IMAGE_URL")
+    private String imageUrl;
+
 }
