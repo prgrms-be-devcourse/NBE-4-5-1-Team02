9dd292aa13da99d685ec60b7ca3023e474dbc332
omegafrog <jiwoo22110103@gmail.com>
2025-02-20 10:26:04 +0900

    feat: response를 위한 RsData 속성 추가

--- src/main/java/com/team2/demo/global/response/RsData.java
+++ src/main/java/com/team2/demo/global/response/RsData.java
@@ -1,20 +1,23 @@
 package com.team2.demo.global.response;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.http.HttpStatus;
 
 @NoArgsConstructor
 @Getter
+@Builder
+@AllArgsConstructor
 public class RsData<T> {
-
-    private static final RsData
 
     private String message;
     private T data;
+    private int code;
 
-    public static <T> RsData<T> success(T data) {
+    public static <T> RsData<T> success(String message, T data) {
+        return new RsData<>(message, data, HttpStatus.OK.value());
+    }
+    public static <T> RsData<T> badRequest(String message) {
 
     }
-    public static <T> RsData<T> badRequest(String message) {}
+
 }
