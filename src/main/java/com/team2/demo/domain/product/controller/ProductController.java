3454ad466caec78d77e8585bc921e4062acc6797
omegafrog <jiwoo22110103@gmail.com>
2025-02-20 11:17:04 +0900

    feat: PaginationData 추가
    페이지네이션된 데이터 응답을 주기 위한 클래스 추가

--- src/main/java/com/team2/demo/domain/product/controller/ProductController.java
+++ src/main/java/com/team2/demo/domain/product/controller/ProductController.java
@@ -3,7 +3,11 @@ package com.team2.demo.domain.product.controller;
 import com.team2.demo.domain.product.dto.ProductDto;
 import com.team2.demo.domain.product.service.ProductService;
 import com.team2.demo.global.response.RsData;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,18 +15,32 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/products")
+@RequestMapping("/products")
 @RequiredArgsConstructor
 public class ProductController {
 
     private final ProductService productService;
 
+    @Builder
+    @AllArgsConstructor
+    public static class PaginationData<T>{
+        private List<T> data;
+        private int page;
+        private int size;
+        private int totalPages;
+    }
+
     @PostMapping
-    public RsData<List<ProductDto>> getProductList(){
-        List<ProductDto> products = productService.getProductList();
+    public RsData<PaginationData<ProductDto>> getProductList(Pageable pageable){
+        Page<ProductDto> products = productService.getProductList(pageable);
         return RsData.success(
                 "성공했습니다.",
-                products
+                        PaginationData.<ProductDto>builder()
+                        .data(products.getContent())
+                        .page(products.getNumber())
+                        .size(products.getSize())
+                        .totalPages(products.getTotalPages())
+                        .build()
         );
     }
 }
