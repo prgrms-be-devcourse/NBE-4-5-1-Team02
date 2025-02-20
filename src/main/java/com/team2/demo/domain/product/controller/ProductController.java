b8abb57f4c0e3d8c4064d328921c8ef690dce15b
omegafrog <jiwoo22110103@gmail.com>
2025-02-20 10:25:39 +0900

    feat: ProductController.getProductList 작성

--- src/main/java/com/team2/demo/domain/product/controller/ProductController.java
+++ src/main/java/com/team2/demo/domain/product/controller/ProductController.java
@@ -1,16 +1,28 @@
 package com.team2.demo.domain.product.controller;
 
+import com.team2.demo.domain.product.dto.ProductDto;
+import com.team2.demo.domain.product.service.ProductService;
 import com.team2.demo.global.response.RsData;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
 
     @PostMapping
     public RsData<List<ProductDto>> getProductList(){
-        return new RsData<>()
+        List<ProductDto> products = productService.getProductList();
+        return RsData.success(
+                "성공했습니다.",
+                products
+        );
     }
 }
