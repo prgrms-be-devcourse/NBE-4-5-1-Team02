67731091a4b0c2557ae99a7dd347a38eb016e413
joung-pjh <jounghyeon123@daum.net>
2025-02-20 10:43:58 +0900

    feat: 사용자 주문 목록 조회 메서드 생성 및 paging 적용

--- src/main/java/com/team2/demo/domain/order/dto/OrderDto.java
+++ src/main/java/com/team2/demo/domain/order/dto/OrderDto.java
@@ -1,4 +1,26 @@
 package com.team2.demo.domain.order.dto;
 
+import com.team2.demo.domain.order.entity.Order;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderDto {
+    private String orderId;
+    private LocalDateTime orderDate;
+    private Integer totalPrice;
+    private String deliveryStatus;
+    private String buyerEmail;
+
+    public OrderDto (Order order) {
+        this.orderId = order.getOrderUuid();
+        this.orderDate = order.getCreateDate();
+        this.totalPrice = order.getTotalAmount();
+        this.deliveryStatus = order.getDeliveryStatus();
+        this.buyerEmail = order.getUser().getEmail();
+    }
 }
--- src/main/java/com/team2/demo/domain/order/service/OrderService.java
+++ src/main/java/com/team2/demo/domain/order/service/OrderService.java
@@ -1,4 +1,17 @@
 package com.team2.demo.domain.order.service;
 
+import com.team2.demo.domain.order.dto.OrderDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class OrderService {
+    private final OrderService orderService;
+
+    public Page<OrderDto> getOrdersByEmail(String email, int page, int size) {
+
+        return null;
+    }
 }
