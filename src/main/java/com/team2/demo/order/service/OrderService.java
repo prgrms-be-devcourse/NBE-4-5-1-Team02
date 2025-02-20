181306b001f8fcc24406a0eccbf8608f42a6b929
hoechanj <jhc400573@gmail.com>
2025-02-20 17:49:25 +0900

    feat: OrderUpdate 추가

--- src/main/java/com/team2/demo/order/service/OrderService.java
+++ src/main/java/com/team2/demo/order/service/OrderService.java
@@ -4,6 +4,7 @@ import com.team2.demo.order.Order;
 import com.team2.demo.order.dto.OrderDto;
 import com.team2.demo.order.repository.OrderRepository;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository orderRepository;
+
+    @Transactional
+    public Order updateOrder(String orderId, Order request) {
+        Order order = orderRepository.findById(orderId).
+                orElseThrow(()-> new EntityNotFoundException("주문을 찾을 수 없습니다: " + orderId));
+
+    }
 
 
 }
