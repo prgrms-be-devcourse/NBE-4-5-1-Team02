139355aaa663a4b80bddc0360a98f87aeb14329a
joung-pjh <jounghyeon123@daum.net>
2025-02-20 10:43:58 +0900

    feat: 사용자 주문 목록 조회 메서드 생성 및 paging 적용

--- src/main/java/com/team2/demo/domain/order/controller/OrderController.java
+++ src/main/java/com/team2/demo/domain/order/controller/OrderController.java
@@ -1,4 +1,28 @@
 package com.team2.demo.domain.order.controller;
 
+import com.team2.demo.domain.order.dto.OrderDto;
+import com.team2.demo.domain.order.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/orders")
 public class OrderController {
+
+    private final OrderService orderService;
+
+    /*
+        사용자 주문 목록 조회
+        GET /orders?email=user@example.com&page=1&size=10
+     */
+    @GetMapping
+    public void getOrders(@PathVariable String email,
+                          @RequestParam(defaultValue = "1") int page,
+                          @RequestParam(defaultValue = "10") int size) {
+
+        Page<OrderDto> orderPage = orderService.getOrdersByEmail(email, page, size);
+
+    }
 }
--- src/main/java/com/team2/demo/domain/order/repository/OrderRepository.java
+++ src/main/java/com/team2/demo/domain/order/repository/OrderRepository.java
@@ -1,4 +1,12 @@
 package com.team2.demo.domain.order.repository;
 
-public class OrderRepository {
+import com.team2.demo.domain.order.entity.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, String> {
+    Page<Order> findAllByUser_Email(String email, Pageable pageable);
 }
