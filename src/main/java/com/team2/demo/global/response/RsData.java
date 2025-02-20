e17ceac01d221120138f29081d5b1a430e590d2c
yunjuKimm <yunju.tech@gmail.com>
2025-02-20 13:38:35 +0900

    feat: response를 위한 RsData 추가

--- src/main/java/com/team2/demo/global/response/RsData.java
+++ src/main/java/com/team2/demo/global/response/RsData.java
@@ -1,20 +1,27 @@
 package com.team2.demo.global.response;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.http.HttpStatus;
 
 @NoArgsConstructor
 @Getter
 public class RsData<T> {
 
-    private static final RsData
-
+    private int resultCode;
     private String message;
     private T data;
 
-    public static <T> RsData<T> success(T data) {
-
+    public RsData(int resultCode, String message, T data) {
+        this.resultCode = resultCode;
+        this.message = message;
+        this.data = data;
     }
-    public static <T> RsData<T> badRequest(String message) {}
+
+    public static <T> RsData<T> success(T data) {
+       return new RsData<>(HttpStatus.OK.value(), "성공", data);
+    }
+
+    public static <T> RsData<T> fail(String message) {
+        return new RsData<>(HttpStatus.BAD_REQUEST.value(), message, null);
+    }
 }
