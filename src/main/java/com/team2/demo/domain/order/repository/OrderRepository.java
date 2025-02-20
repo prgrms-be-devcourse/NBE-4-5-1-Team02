8a1f42ba013956e98778d8fa93548127b88c42da
yunjuKimm <yunju.tech@gmail.com>
2025-02-20 11:35:40 +0900

    refactor: OrderRepository 인터페이스로 변경

--- src/main/java/com/team2/demo/domain/order/repository/OrderRepository.java
+++ src/main/java/com/team2/demo/domain/order/repository/OrderRepository.java
@@ -1,4 +1,9 @@
 package com.team2.demo.domain.order.repository;
 
-public class OrderRepository {
+import com.team2.demo.domain.order.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface OrderRepository extends JpaRepository<Order, String> {
+    Optional<Order> findById(String orderUuid);
 }
