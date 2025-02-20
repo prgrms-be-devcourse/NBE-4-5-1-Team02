3b7cb6e55ebc17e3b4586e15df3567416f4dc291
yunjuKimm <yunju.tech@gmail.com>
2025-02-20 12:24:08 +0900

    feat: OrderController.updateOrder() 추가

--- src/main/java/com/team2/demo/domain/order/controller/OrderController.java
+++ src/main/java/com/team2/demo/domain/order/controller/OrderController.java
@@ -1,4 +1,27 @@
 package com.team2.demo.domain.order.controller;
 
+import com.team2.demo.domain.order.dto.OrderDto;
+import com.team2.demo.domain.order.service.OrderService;
+import com.team2.demo.global.response.RsData;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/orders")
 public class OrderController {
+
+    @Autowired
+    private final OrderService orderService;
+
+    @putMapping("/{orderId}")
+    public ResponseEntity<RsData<OrderDto>> updateOrder(
+            @PathVariable String orderId,
+            @RequestParam String email,
+            @RequestBody OrderDto request) {
+        return ResponseEntity.ok(RsData.success(orderService.updateOrder(orderId, orderDto)));
+    }
 }
--- src/main/java/com/team2/demo/domain/order/repository/OrderRepository.java
+++ src/main/java/com/team2/demo/domain/order/repository/OrderRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, String> {
-    Optional<Order> findById(String orderUuid);
+    Optional<Order> findByOrderUuid(String orderUuid);
 }
