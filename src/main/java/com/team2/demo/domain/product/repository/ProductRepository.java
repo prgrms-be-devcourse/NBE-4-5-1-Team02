397bb7ac04a675636707b3c9d48a7eb2a979e25c
omegafrog <jiwoo22110103@gmail.com>
2025-02-20 12:29:04 +0900

    feat: 검색을 위한 repository interface method 추가

--- src/main/java/com/team2/demo/domain/product/repository/ProductRepository.java
+++ src/main/java/com/team2/demo/domain/product/repository/ProductRepository.java
@@ -1,7 +1,10 @@
 package com.team2.demo.domain.product.repository;
 
 import com.team2.demo.domain.product.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findAllByCategoryLike(String category, Pageable pageable);
 }
