419afe9f4a74df66963463e22c35ce0abaf1af97
joung-pjh <jounghyeon123@daum.net>
2025-02-21 17:29:38 +0900

    test: OrderControllerTest 테스트 클래스-1

--- backend/src/test/java/com/team2/demo/domain/order/controller/OrderControllerTest.java
+++ backend/src/test/java/com/team2/demo/domain/order/controller/OrderControllerTest.java
@@ -1,12 +1,25 @@
 package com.team2.demo.domain.order.controller;
 
-import org.junit.jupiter.api.Test;
+import com.team2.demo.domain.order.dto.OrderDto;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.ResultActions;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
 class OrderControllerTest {
 
-    @Test
-    void getOrders() {
+    private void checkOrder(ResultActions resultActions, OrderDto orderDto) throws Exception {
+        resultActions
+                .andExpect(jsonPath("$.data").exists())
+                .andExpect(jsonPath("$.data.orderId").value(orderDto.getOrderId()))
+                .andExpect(jsonPath("$.data.orderDate").value(orderDto.getOrderDate().toString()))
+                .andExpect(jsonPath("$.data.totalPrice").value(orderDto.getTotalPrice()))
+                .andExpect(jsonPath("$.data.deliveryStatus").value(orderDto.getDeliveryStatus().toString()))
+                .andExpect(jsonPath("$.data.buyerEmail").value(orderDto.getBuyerEmail()))
+                .andExpect(jsonPath("$.data.createdDate").value(matchesPattern(orderDto.getCreatedDate().toString().replaceAll("0+$", "") + ".*")))
+                .andExpect(jsonPath("$.data.modifiedDate").value(matchesPattern(orderDto.getModifiedDate().toString().replaceAll("0+$", "") + ".*")));
     }
 }
