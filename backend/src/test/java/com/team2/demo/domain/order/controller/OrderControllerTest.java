e45ba5e83892947c22bdc4886d8aa662fcff8864
joung-pjh <jounghyeon123@daum.net>
2025-02-21 17:19:49 +0900

    test: OrderControllerTest 테스트 클래스 생성

---
+++ backend/src/test/java/com/team2/demo/domain/order/controller/OrderControllerTest.java
@@ -0,0 +1,12 @@
+package com.team2.demo.domain.order.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderControllerTest {
+
+    @Test
+    void getOrders() {
+    }
+}
