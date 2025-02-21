f4a0760b90c1f37f8fd953815874eb38911d224c
joung-pjh <jounghyeon123@daum.net>
2025-02-21 22:01:39 +0900

    test: OrderControllerAdminTest 테스트 코드 작성(완료) + orderDate 검증은 주문 시간과 생성시간에 차이가 있어 불가.(방법 고안 필요)

--- backend/src/test/java/com/team2/demo/domain/order/controller/OrderControllerAdminTest.java
+++ backend/src/test/java/com/team2/demo/domain/order/controller/OrderControllerAdminTest.java
@@ -54,6 +54,7 @@ class OrderControllerAdminTest {
         // 초기 데이터
         int page = 1;
         int size = 10;
+        int maxItems = 2;
 
         ResultActions resultActions = mvc
                 .perform(get("/admin/orders")
@@ -65,17 +66,17 @@ class OrderControllerAdminTest {
 
         resultActions
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.code").value(200))  // code 값 검증
-                .andExpect(jsonPath("$.message").value("ok"))  // message 값 검증
-                .andExpect(jsonPath("$.data.content.length()").value(5))  // content 배열의 길이 검증
-                .andExpect(jsonPath("$.data.page").value(1))  // 페이지 번호 검증
-                .andExpect(jsonPath("$.data.size").value(10))  // 한 페이지에 포함된 항목 수 검증
-                .andExpect(jsonPath("$.data.totalPages").value(1));  // 전체 페이지 수 검증
+                .andExpect(jsonPath("$.code").value(200))  // 응답 코드가 200인지 확인
+                .andExpect(jsonPath("$.message").value("ok"))  // message 필드가 "ok"인지 확인
+                .andExpect(jsonPath("$.data.content.length()").value(5))  // content 배열에 5개의 주문이 있는지 확인
+                .andExpect(jsonPath("$.data.page").value(1))  // 페이지 번호가 1인지 확인
+                .andExpect(jsonPath("$.data.size").value(10))  // 한 페이지에 포함된 항목 수가 10인지 확인
+                .andExpect(jsonPath("$.data.totalPages").value(1));  // 전체 페이지 수가 1인지 확인
 
         // 각 주문 항목에 대한 검증
         resultActions
                 .andExpect(jsonPath("$.data.content[0].orderId").value("order-11111-22222-33335"))
-                .andExpect(jsonPath("$.data.content[0].orderDate").value("2025-02-21T16:15:35.3138"))
+//                .andExpect(jsonPath("$.data.content[0].orderDate").value("2025-02-21T21:56:50.719344"))
                 .andExpect(jsonPath("$.data.content[0].totalPrice").value(30000))
                 .andExpect(jsonPath("$.data.content[0].deliveryStatus").value("DELIVERED"))
                 .andExpect(jsonPath("$.data.content[0].buyerEmail").value("email3@email.com"))
@@ -83,7 +84,7 @@ class OrderControllerAdminTest {
 
         resultActions
                 .andExpect(jsonPath("$.data.content[1].orderId").value("order-11111-22222-33334"))
-                .andExpect(jsonPath("$.data.content[1].orderDate").value("2025-02-21T16:15:35.30809"))
+//                .andExpect(jsonPath("$.data.content[1].orderDate").value("2025-02-21T21:56:50.719344"))
                 .andExpect(jsonPath("$.data.content[1].totalPrice").value(20000))
                 .andExpect(jsonPath("$.data.content[1].deliveryStatus").value("SHIPPED"))
                 .andExpect(jsonPath("$.data.content[1].buyerEmail").value("email2@email.com"))
@@ -91,7 +92,7 @@ class OrderControllerAdminTest {
 
         resultActions
                 .andExpect(jsonPath("$.data.content[2].orderId").value("order-11111-22222-33333"))
-                .andExpect(jsonPath("$.data.content[2].orderDate").value("2025-02-21T16:15:35.306558"))
+//                .andExpect(jsonPath("$.data.content[2].orderDate").value("2025-02-21T21:56:50.719344"))
                 .andExpect(jsonPath("$.data.content[2].totalPrice").value(10000))
                 .andExpect(jsonPath("$.data.content[2].deliveryStatus").value("SHIPPED"))
                 .andExpect(jsonPath("$.data.content[2].buyerEmail").value("email2@email.com"))
@@ -99,7 +100,7 @@ class OrderControllerAdminTest {
 
         resultActions
                 .andExpect(jsonPath("$.data.content[3].orderId").value("order-11111-22222-33332"))
-                .andExpect(jsonPath("$.data.content[3].orderDate").value("2025-02-21T16:15:35.305561"))
+//                .andExpect(jsonPath("$.data.content[3].orderDate").value("2025-02-21T21:56:50.718345"))
                 .andExpect(jsonPath("$.data.content[3].totalPrice").value(20000))
                 .andExpect(jsonPath("$.data.content[3].deliveryStatus").value("PENDING"))
                 .andExpect(jsonPath("$.data.content[3].buyerEmail").value("email1@email.com"))
@@ -107,16 +108,17 @@ class OrderControllerAdminTest {
 
         resultActions
                 .andExpect(jsonPath("$.data.content[4].orderId").value("order-11111-22222-33331"))
-                .andExpect(jsonPath("$.data.content[4].orderDate").value("2025-02-21T16:15:35.304557"))
+//                .andExpect(jsonPath("$.data.content[4].orderDate").value("2025-02-21T21:56:50.718345"))
                 .andExpect(jsonPath("$.data.content[4].totalPrice").value(10000))
                 .andExpect(jsonPath("$.data.content[4].deliveryStatus").value("PENDING"))
                 .andExpect(jsonPath("$.data.content[4].buyerEmail").value("email1@email.com"))
                 .andExpect(jsonPath("$.data.content[4].items.length()").value(2))  // 다섯 번째 주문의 items 배열 길이 검증
                 .andExpect(jsonPath("$.data.content[4].items[0].name").value("product2"))
-                .andExpect(jsonPath("$.data.content[4].items[0].quantity").value(1))
+                .andExpect(jsonPath("$.data.content[4].items[0].quantity").value(1))  // 첫 번째 상품 검증
                 .andExpect(jsonPath("$.data.content[4].items[1].name").value("product1"))
-                .andExpect(jsonPath("$.data.content[4].items[1].quantity").value(5));  // 두 개의 상품이 있는지 검증
+                .andExpect(jsonPath("$.data.content[4].items[1].quantity").value(5));  // 두 번째 상품 검증
     }
+
 
 
 }
