775de334b75581d7b850f09ee02fa4b08d1dc4c6
omegafrog <jiwoo22110103@gmail.com>
2025-02-21 17:58:46 +0900

    bug: category가 정상적으로 변경되도록 수정

--- backend/src/main/java/com/team2/demo/domain/product/service/ProductService.java
+++ backend/src/main/java/com/team2/demo/domain/product/service/ProductService.java
@@ -2,6 +2,7 @@ package com.team2.demo.domain.product.service;
 
 import com.team2.demo.domain.product.dto.ProductDto;
 import com.team2.demo.domain.product.repository.ProductRepository;
+import com.team2.demo.global.exception.prodcut.UnsupportedProductKeywordTypeException;
 import jakarta.transaction.Transactional;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.RequiredArgsConstructor;
@@ -20,8 +21,12 @@ public class ProductService {
 
     public Page<ProductDto> getProductList(String keywordType, String keyword, Pageable pageable) {
 
-        ProductKeywordType searchKeywordType = ProductKeywordType.valueOf(keywordType.toUpperCase());
-        return searchKeywordType.getSearchResult(productRepository, keyword, pageable);
+        try{
+            ProductKeywordType searchKeywordType = ProductKeywordType.valueOf(keywordType.toUpperCase());
+            return searchKeywordType.getSearchResult(productRepository, keyword, pageable);
+        }catch (IllegalArgumentException e){
+            throw new UnsupportedProductKeywordTypeException("지원하지 않는 검색 키워드 타입입니다.");
+        }
     }
 
     public Page<ProductDto> getProductsInOrder(@NotEmpty String orderId, Pageable pageable) {
