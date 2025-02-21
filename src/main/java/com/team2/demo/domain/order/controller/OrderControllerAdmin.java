e1448fd73770eea71970fa0970df081f56dbf12e
hoechanj <jhc400573@gmail.com>
2025-02-21 13:09:26 +0900

    refactor: review변경사항 수정

--- src/main/java/com/team2/demo/domain/order/controller/OrderControllerAdmin.java
+++ src/main/java/com/team2/demo/domain/order/controller/OrderControllerAdmin.java
@@ -8,7 +8,6 @@ import com.team2.demo.global.response.OrderListResponse;
 import com.team2.demo.global.response.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
