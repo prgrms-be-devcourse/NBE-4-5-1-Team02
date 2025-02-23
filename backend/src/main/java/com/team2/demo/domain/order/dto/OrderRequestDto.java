ea53fd2f87a8ede03f2b07f82d5d098e4608a9f9
Kim Ji Woo <jiwoo22110103@gmail.com>
2025-02-23 11:56:56 +0900

    Revert "[bug] : 사용자 주문 생성시 order에 product가 들어가지 않는 오류"

--- backend/src/main/java/com/team2/demo/domain/order/dto/OrderRequestDto.java
+++ backend/src/main/java/com/team2/demo/domain/order/dto/OrderRequestDto.java
@@ -3,6 +3,7 @@ package com.team2.demo.domain.order.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.team2.demo.domain.order.entity.Order;
 import com.team2.demo.domain.product.dto.ProductListDto;
+import com.team2.demo.domain.user.entity.User;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -12,6 +13,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
