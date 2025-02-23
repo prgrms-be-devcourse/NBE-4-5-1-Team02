f499aa7f5df7a3e653e0e3b1e4eb3b868821ea03
omegafrog <jiwoo22110103@gmail.com>
2025-02-23 18:07:11 +0900

    FE - feat: 입력정보 input 스타일 설정

--- frontend/src/app/home/UserDataInput.tsx
+++ frontend/src/app/home/UserDataInput.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Input } from "@/components/ui/input";
+
 export default function UserDataInput({
   emailStatus,
   addressStatus,
@@ -10,34 +12,49 @@ export default function UserDataInput({
   zipCodeStatus: [string, React.Dispatch<React.SetStateAction<string>>];
 }) {
   return (
-    <>
-      <label htmlFor="email">email</label>
-      <input
+    <div className="h-[40%]">
+      <label htmlFor="email">
+        <div className="w-[100%] py-3">
+          <span className="text-2xl font-semibold">email</span>
+        </div>
+      </label>
+      <Input
+        className="bg-white h-[3rem]"
         id="email"
         value={emailStatus[0]}
         onChange={(e) => {
           e.preventDefault();
           emailStatus[1](e.target.value);
         }}
-      ></input>
-      <label htmlFor="address">주소</label>
-      <input
+      ></Input>
+      <label htmlFor="address">
+        <div className="w-[100%] py-3">
+          <span className="text-2xl font-semibold">주소</span>
+        </div>
+      </label>
+      <Input
         id="address"
+        className="bg-white h-[3rem]"
         value={addressStatus[0]}
         onChange={(e) => {
           e.preventDefault();
           addressStatus[1](e.target.value);
         }}
-      ></input>
-      <label htmlFor="zipcode">우편번호</label>
-      <input
+      ></Input>
+      <label htmlFor="zipcode">
+        <div className="w-[100%] py-3">
+          <span className="text-2xl font-semibold">우편번호</span>
+        </div>
+      </label>
+      <Input
         id="zipcode"
+        className="bg-white h-[3rem]"
         value={zipCodeStatus[0]}
         onChange={(e) => {
           e.preventDefault();
           zipCodeStatus[1](e.target.value);
         }}
       />
-    </>
+    </div>
   );
 }
